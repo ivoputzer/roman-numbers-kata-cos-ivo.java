c3f6d9ff6ab926ffe9e6a53fd50c9889d2344b1f
Ivo von Putzer Reibegg <ivo.putzer@gmail.com>
2018-04-24 15:02:46 +0200

    adds converter for numbers from zero to six

--- src/main/java/RomanNumberConverter.java
+++ src/main/java/RomanNumberConverter.java
@@ -1,7 +1,18 @@
 class RomanNumberConverter {
     String toRoman(int number) {
-        if(number == 1)
-            return "I";
-        return "II";
+
+        if (number == 6)
+            return "V" + toRoman(1);
+
+        if (number == 5)
+            return "V";
+
+        if (number == 4)
+            return toRoman(1) + "V";
+
+        if (number > 0)
+            return "I" + toRoman(number-1);
+
+        return "";
     }
 }
--- src/test/java/RomanNumberConverterTest.java
+++ src/test/java/RomanNumberConverterTest.java
@@ -3,7 +3,12 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class RomanNumberConverterTest {
-    final RomanNumberConverter converter = new RomanNumberConverter();
+    private final RomanNumberConverter converter = new RomanNumberConverter();
+
+    @Test
+    public void evaluates_number_zero() {
+        assertEquals("", converter.toRoman(0));
+    }
 
     @Test
     public void evaluates_number_one() {
@@ -13,5 +18,25 @@ public class RomanNumberConverterTest {
     @Test
     public void evaluates_number_two() {
         assertEquals("II", converter.toRoman(2));
+    }
+
+    @Test
+    public void evaluates_number_three() {
+        assertEquals("III", converter.toRoman(3));
+    }
+
+    @Test
+    public void evaluates_number_four() {
+        assertEquals("IV", converter.toRoman(4));
+    }
+
+    @Test
+    public void evaluates_number_five() {
+        assertEquals("V", converter.toRoman(5));
+    }
+
+    @Test
+    public void evaluates_number_six() {
+        assertEquals("VI", converter.toRoman(6));
     }
 }
