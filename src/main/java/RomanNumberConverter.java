f26061b2b76faee0f3a9d5640d93cbb155e84344
Ivo von Putzer Reibegg <ivo.putzer@gmail.com>
2018-04-24 16:01:07 +0200

    replaces n-1 values with hashmap

--- src/main/java/RomanNumberConverter.java
+++ src/main/java/RomanNumberConverter.java
@@ -1,18 +1,28 @@
+import java.net.Inet4Address;
+import java.util.HashMap;
+import java.util.Map;
+
 class RomanNumberConverter {
 
-    String toRoman(int number) {
+    Map link = new HashMap<Integer, String>() {{
+        put(0, "");
+        put(1, "I");
+        put(4, "IV");
+        put(5, "V");
+        put(9, "IX");
+        put(10, "X");
+    }};
+
+    String toRoman(Integer number) {
+
+        if (link.containsKey(number))
+            return String.valueOf(link.get(number));
 
         if (number >= 10)
             return "X" + toRoman(number - 10);
 
-        if (number >= 9)
-            return toRoman(1) + "X";
-
         if (number >= 5)
             return "V" + toRoman(number - 5);
-
-        if (number >= 4)
-            return toRoman(1) + "V";
 
         if (number >= 1)
             return "I" + toRoman(number - 1);
